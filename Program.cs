ae5eb1c86f15d6a3a1b2b2f158d6d8674eb9cab9
Luiza <monalyiza@mail.ru>
2022-11-16 10:26:30 +0900

    написана программа

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿string[] arrayOfElementsLessThanThree (string[] incomingArray)
+{
+    string[] currentArray = new string [incomingArray.Length];
+    int index = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if(incomingArray[i].Length <= 3)
+        {
+        currentArray[index] = incomingArray[i];
+        index++;
+        }
+    }
+    Array.Resize(ref currentArray, index);
+    return currentArray;
+}
+void printArray(string[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if(i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+string[] array = new string[5] {"one", "two", "three", "four", "five"};
+string[] currentArray = arrayOfElementsLessThanThree (array);
+printArray(currentArray);
+
+
